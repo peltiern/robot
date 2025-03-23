5e55e8fc4cf5c98c4f2ac424122200fb8ef554dc
roboteek <nico.peltier@gmail.com>
2025-03-23 18:25:23 +0100

    Montée de version de librairies dont SpringBoot (#61)

--- robot-core/src/main/java/fr/roboteek/robot/spring/server/config/SecurityConfig.java
+++ robot-core/src/main/java/fr/roboteek/robot/spring/server/config/SecurityConfig.java
@@ -2,34 +2,31 @@ package fr.roboteek.robot.spring.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .and()
-                .headers()
-                .frameOptions().disable()
-                .and()
-                .csrf().disable()
-                .authorizeRequests()
-                // TODO Gérer la sécurité de l'application Angular (pour l'instant, c'est open-bar !!!)
-                // Autorisation du endpoint pour le handshake de STOMP
-//                .antMatchers("/wsendpoint").permitAll()
-                .antMatchers("*").permitAll();
-//                .anyRequest()
-//                .authenticated();
-        // @formatter:on
+                .cors(Customizer.withDefaults())
+                .headers(headers -> headers.frameOptions(Customizer.withDefaults()).disable()) // Désactive les restrictions sur les frames
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        //.requestMatchers("/wsendpoint").permitAll()
+                        .requestMatchers("/**").permitAll()
+                );
+
+        return http.build();
     }
 
     /**
